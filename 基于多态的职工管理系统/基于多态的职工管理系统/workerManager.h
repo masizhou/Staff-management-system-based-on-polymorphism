99969796d1e54d954d435fc15183957c8e5af2d3
Ma Sizhou <1907609069@qq.com>
2021-04-06 23:30:09 +0800

    初始菜单的开发

---
+++ 基于多态的职工管理系统/基于多态的职工管理系统/workerManager.h
@@ -0,0 +1,19 @@
+#pragma once  //防止头文件重复包含
+#include <iostream> //包含输入输出流的头文件
+using namespace std; //使用标准命名空间
+
+class WorkerManager
+{
+public:
+	//构造函数
+	WorkerManager();
+
+	//展示菜单
+	void Show_Menu();
+
+	//推出系统
+	void ExitSystem();
+
+	//析构函数
+	~WorkerManager();
+};
